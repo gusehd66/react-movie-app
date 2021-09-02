4f0b5679cc814d72af4b3881583db38b8946a85e
gusehd66 <rlagusehd45@naver.com>
2021-09-02 18:35:46 +0900

    Home component 무한 로딩 오류 처리

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -16,7 +16,7 @@ class Home extends React.Component {
         data: { movies },
       }
     } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
-    this.setState({ movies, isLoading: true });
+    this.setState({ movies, isLoading: false });
   }
   componentDidMount() {
     this.getMovies();
