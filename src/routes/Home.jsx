2182665493db9e1272c621203001f3178e87b442
gusehd66 <rlagusehd45@naver.com>
2021-09-08 10:58:47 +0900

    Mainpage 컴포넌트 Home 컴포넌트로 통합

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PageContext from '../context/PageContext';
-import MainPage from '../component/MainPage'
 import './Home.css'
+import axios from 'axios';
+import Loader from '../component/Loader';
+import Movie from '../component/Movie';
 
 function Home() {
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    setLoading(true);
+    const {
+      data: {
+        data: { movies },
+      }
+    } =
+      await axios.get(`https://yts.mx/api/v2/list_movies.json?limit=20&sort_by=download_count&page=${page}`);
+    setMovies(movies);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, [page]);
+
   const nextPage = () => {
     setPage(page + 1);
   }
@@ -16,11 +37,34 @@ function Home() {
   }
 
   return (
-    <PageContext.Provider value={{ page }}>
-      <button id="preBtn" onClick={prePage}>Pre Page</button>
-      <button id="nextBtn" onClick={nextPage}>Next Page</button>
-      <MainPage />
-    </PageContext.Provider>
+    <section className="container">
+      {loading ? (
+        <div className="loader">
+          <Loader />
+        </div>
+      ) : (
+        <div className="movies">
+          <button id="preBtn" onClick={prePage}>Pre Page</button>
+          <button id="nextBtn" onClick={nextPage}>Next Page</button>
+          {
+            movies.map((movie) => {
+              return <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+                lgPoster={movie.large_cover_image}
+                rating={movie.rating}
+                runTime={movie.runtime}
+              />;
+            })
+          }
+        </div>
+      )}
+    </section>
   )
 }
 
